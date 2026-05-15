04d5cfaff75d19fc3b15601b4de21838b155514e
Linux Adona <linuxadona17@gmail.com>
2026-05-15 14:57:30 +0800

    skills script

---
+++ skills.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+npx skills add https://github.com/vercel-labs/skills --skill find-skills
+npx skills add vercel-labs/agent-skills
+npx skills add vercel-labs/agent-browser
+npx skills add vercel-labs/next-skills
+npx skills add https://github.com/anthropics/skills --skill frontend-design
+npx skills add https://github.com/anthropics/skills --skill skill-creator
+npx skills add https://github.com/anthropics/skills --skill pdf
+npx skills add https://github.com/anthropics/skills --skill pptx
+npx skills add https://github.com/anthropics/skills --skill docx
+npx skills add https://github.com/anthropics/skills --skill xlsx
+npx skills add https://github.com/anthropics/skills --skill webapp-testing
+npx skills add https://github.com/anthropics/skills --skill doc-coauthoring
